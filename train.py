25444b8fbaa84d48d6225f3275d7a848cca4f807
Pratima Gokhale <pratima.gokhale@gmail.com>
2020-05-04 17:42:54 -0500

    The train.py is updated with the final model

--- train.py
+++ train.py
@@ -1,30 +1,34 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
 from joblib import dump
 from preprocess import prep_data
 from sklearn.model_selection import cross_validate
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import KFold
 
-df = pd.read_csv("fish_participant.csv").set_index("Species")
+df = pd.read_csv("fish_participant.csv")
 
 print(df.head)
 print(df.dtypes)
 
 X, y = prep_data(df)
 
-lr = LinearRegression()
-print(cross_validate(lr, X,y, scoring ="neg_mean_squared_error"))
-# lr.fit(X, y)
+dt = DecisionTreeRegressor()
+cross_validate(dt, X,y, scoring ="neg_mean_squared_error",
+              cv=KFold(random_state=123, shuffle=True))['test_score'].mean()
+dt.fit(X,y)
+# dump(dt, 'dt.joblib') 
 
 
-# df_holdout = pd.read_csv("fish_holdout_demo.csv").set_index("Species")
-# X_hold, y_true = prep_data(df_holdout)
+df_holdout = pd.read_csv("fish_holdout_demo.csv")
+X_hold, y_true = prep_data(df_holdout)
 
-# y_predict = lr.predict(X_hold)
-# print([y_true, y_predict ])
+y_predict = dt.predict(X_hold)
+print([y_true, y_predict ])
 
-# print(mean_squared_error(y_true, y_predict))
+print(mean_squared_error(y_true, y_predict))
 
 
-# dump(lr, "reg.joblib")
+# dump(dt, "dt.joblib")
